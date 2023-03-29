f9d14801ca28c8ad821f80ee2afb271df376671b
MartMbithi <martdevelopers254@gmail.com>
2023-03-29 15:11:26 +0300

    Implement Favicons

--- partials/head.php
+++ partials/head.php
@@ -5,11 +5,15 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Property Manager | Your Personal Property Manager Assistant </title>
-
+    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo $base_dir; ?>../public/img/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo $base_dir; ?>../public/img/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo $base_dir; ?>../public/img/favicon/favicon-16x16.png">
+    <link rel="manifest" href="<?php echo $base_dir; ?>../public/favicon/site.webmanifest">
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <!-- Font Awesome -->
     <link rel="stylesheet" href="<?php echo $base_dir; ?>../public/plugins/fontawesome-free/css/all.min.css">
+    <!-- Bootstrap -->
     <link rel="stylesheet" href="<?php echo $base_dir; ?>../public/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
     <!-- iCheck -->
     <link rel="stylesheet" href="<?php echo $base_dir; ?>../public/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
