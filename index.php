6ca1e26cc7fe52e00383d9a990a1a05faddeb86c
MartMbithi <martdevelopers254@gmail.com>
2023-03-29 05:53:30 +0300

    Craft Auth Pages

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
+    $uri = 'https://';
+} else {
+    $uri = 'http://';
+}
+$uri .= $_SERVER['HTTP_HOST'];
+/* Redirect To Index Under Views */
+header('Location: ' . $uri . '/property_manager/views/login');
+exit;
---
+++ views/login.php
@@ -0,0 +1,67 @@
+<?php
+/*
+ *   Crafted On Wed Mar 29 2023
+ *   Author Martin (martin@devlan.co.ke)
+ * 
+ */
+session_start();
+require_once('../partials/head.php');
+?>
+
+<body class="hold-transition login-page">
+    <div class="login-box">
+        <!-- /.login-logo -->
+        <div class="card card-outline card-primary">
+            <div class="card-header text-center">
+                <a href="" class="h1"><b>Property</b> Manager</a>
+            </div>
+            <div class="card-body">
+                <p class="login-box-msg">Sign in to start your session</p>
+
+                <form action="../../index3.html" method="post">
+                    <div class="input-group mb-3">
+                        <input type="email" required class="form-control" name="user_login_name" placeholder="Email">
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-envelope"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="input-group mb-3">
+                        <input type="password" required class="form-control" name="user_password" placeholder="Password">
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-lock"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-8">
+                            <div class="icheck-primary">
+                                <input type="checkbox" id="remember">
+                                <label for="remember">
+                                    Remember Me
+                                </label>
+                            </div>
+                        </div>
+                        <!-- /.col -->
+                        <div class="col-4">
+                            <button type="submit" name="login" class="btn btn-primary btn-block">Sign In</button>
+                        </div>
+                        <!-- /.col -->
+                    </div>
+                </form>
+
+                <p class="mb-1">
+                    <a href="forgot-password.html">I forgot my password</a>
+                </p>
+            </div>
+            <!-- /.card-body -->
+        </div>
+        <!-- /.card -->
+    </div>
+    <!-- /.login-box -->
+    <?php require_once('../partials/scripts.php'); ?>
+</body>
+
+</html>
