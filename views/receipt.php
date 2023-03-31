51698b3fe5559c1ea113646c7e48c7c0205f23b3
MartMbithi <martdevelopers254@gmail.com>
2023-03-31 16:17:47 +0300

    Implement Payment Details

--- views/receipt.php
+++ views/receipt.php
@@ -53,127 +53,101 @@ require_once('../partials/head.php');
                     <!-- Main content -->
                     <div class="content">
                         <div class="container">
-                            <div class="col-12">
-                                <div class="card card-outline card-success">
-                                    <div class="card-body">
-                                        <div class="row invoice-info">
-                                            <div class="col-sm-12 invoice-col text-right">
-                                                <address>
-                                                    <strong> <?php echo $payments['tenant_name']; ?></strong><br>
-                                                    Phone:  <?php echo $payments['tenant_mobile_number']; ?><br>
-                                                </address>
-                                                <b>Invoice # <?php echo $payments['payment_invoice_number']; ?></b><br>
-                                                <b>Payment # :</b> <?php echo $payments['payment_ref_code']; ?><br>
-                                                <b>Payment Date:</b> <?php echo date('d M Y', strtotime($payments['payment_date'])); ?><br>
+                            <div class="row">
+                                <div class="col-md-3">
+                                    <!-- Profile Image -->
+                                    <div class="card card-success card-outline">
+                                        <div class="card-body box-profile">
+                                            <div class="text-center">
+                                                <img class="profile-user-img img-fluid img-circle" src="../public/img/avatar.png" alt="User profile picture">
                                             </div>
-                                            <!-- /.col -->
-                                        </div>
-                                        <!-- /.row -->
-
-                                        <!-- Table row -->
-                                        <div class="row">
-                                            <div class="col-12 table-responsive">
-                                                <table class="table table-striped">
-                                                    <thead>
-                                                        <tr>
-                                                            <th>Qty</th>
-                                                            <th>Product</th>
-                                                            <th>Serial #</th>
-                                                            <th>Description</th>
-                                                            <th>Subtotal</th>
-                                                        </tr>
-                                                    </thead>
-                                                    <tbody>
-                                                        <tr>
-                                                            <td>1</td>
-                                                            <td>Call of Duty</td>
-                                                            <td>455-981-221</td>
-                                                            <td>El snort testosterone trophy driving gloves handsome</td>
-                                                            <td>$64.50</td>
-                                                        </tr>
-                                                        <tr>
-                                                            <td>1</td>
-                                                            <td>Need for Speed IV</td>
-                                                            <td>247-925-726</td>
-                                                            <td>Wes Anderson umami biodiesel</td>
-                                                            <td>$50.00</td>
-                                                        </tr>
-                                                        <tr>
-                                                            <td>1</td>
-                                                            <td>Monsters DVD</td>
-                                                            <td>735-845-642</td>
-                                                            <td>Terry Richardson helvetica tousled street art master</td>
-                                                            <td>$10.70</td>
-                                                        </tr>
-                                                        <tr>
-                                                            <td>1</td>
-                                                            <td>Grown Ups Blue Ray</td>
-                                                            <td>422-568-642</td>
-                                                            <td>Tousled lomo letterpress</td>
-                                                            <td>$25.99</td>
-                                                        </tr>
-                                                    </tbody>
-                                                </table>
-                                            </div>
-                                            <!-- /.col -->
-                                        </div>
-                                        <!-- /.row -->
-
-                                        <div class="row">
-                                            <!-- accepted payments column -->
-                                            <div class="col-6">
-                                                <p class="lead">Payment Methods:</p>
-                                                <img src="../../dist/img/credit/visa.png" alt="Visa">
-                                                <img src="../../dist/img/credit/mastercard.png" alt="Mastercard">
-                                                <img src="../../dist/img/credit/american-express.png" alt="American Express">
-                                                <img src="../../dist/img/credit/paypal2.png" alt="Paypal">
-
-                                                <p class="text-muted well well-sm shadow-none" style="margin-top: 10px;">
-                                                    Etsy doostang zoodles disqus groupon greplin oooj voxy zoodles, weebly ning heekya handango imeem
-                                                    plugg
-                                                    dopplr jibjab, movity jajah plickers sifteo edmodo ifttt zimbra.
-                                                </p>
-                                            </div>
-                                            <!-- /.col -->
-                                            <div class="col-6">
-                                                <p class="lead">Amount Due 2/22/2014</p>
-
-                                                <div class="table-responsive">
-                                                    <table class="table">
-                                                        <tr>
-                                                            <th style="width:50%">Subtotal:</th>
-                                                            <td>$250.30</td>
-                                                        </tr>
-                                                        <tr>
-                                                            <th>Tax (9.3%)</th>
-                                                            <td>$10.34</td>
-                                                        </tr>
-                                                        <tr>
-                                                            <th>Shipping:</th>
-                                                            <td>$5.80</td>
-                                                        </tr>
-                                                        <tr>
-                                                            <th>Total:</th>
-                                                            <td>$265.24</td>
-                                                        </tr>
-                                                    </table>
-                                                </div>
-                                            </div>
-                                            <!-- /.col -->
+                                            <h3 class="profile-username text-center"><?php echo $payments['tenant_name']; ?></h3>
+                                            <ul class="list-group list-group-unbordered mb-3">
+                                                <li class="list-group-item">
+                                                    <b>ID Number: </b> <a class="float-right"><?php echo $payments['tenant_national_id']; ?></a>
+                                                </li>
+                                                <li class="list-group-item">
+                                                    <b>Phone Number</b> <a class="float-right"><?php echo $payments['tenant_mobile_number']; ?></a>
+                                                </li>
+                                                <li class="list-group-item">
+                                                    <b>Date Registered: </b> <a class="float-right"><?php echo date('d M Y', strtotime($payments['tenant_date_of_registration'])); ?></a>
+                                                </li>
+                                            </ul>
                                         </div>
                                     </div>
-                                    <!-- /.row -->
-                                    <div class="card-footer">
-                                        <!-- this row will not appear when printing -->
-                                        <div class="row no-print">
-                                            <div class="col-12">
-                                                <a href="invoice-print.html" target="_blank" class="btn btn-default"><i class="fas fa-print"></i> Print</a>
-                                                <button type="button" class="btn btn-success float-right"><i class="far fa-credit-card"></i> Submit
-                                                    Payment
-                                                </button>
-                                                <button type="button" class="btn btn-primary float-right" style="margin-right: 5px;">
-                                                    <i class="fas fa-download"></i> Generate PDF
-                                                </button>
+                                </div>
+                                <div class="col-md-9">
+                                    <div class="card card-success card-outline">
+                                        <div class="card-body">
+                                            <div class="row invoice-info">
+                                                <div class="col-sm-12 invoice-col text-right">
+                                                    <b>Invoice # <?php echo $payments['payment_invoice_number']; ?></b><br>
+                                                    <b>Payment # :</b> <?php echo $payments['payment_ref_code']; ?><br>
+                                                    <b>Payment Date:</b> <?php echo date('d M Y', strtotime($payments['payment_date'])); ?><br>
+                                                </div>
+                                                <!-- /.col -->
+                                            </div>
+                                            <!-- /.row -->
+
+                                            <!-- Table row -->
+                                            <div class="row">
+                                                <div class="col-12 table-responsive">
+                                                    <table class="table table-striped text-right">
+                                                        <thead>
+                                                            <tr>
+                                                                <th>Qty</th>
+                                                                <th>Product</th>
+                                                                <th>Description</th>
+                                                                <th>Subtotal</th>
+                                                            </tr>
+                                                        </thead>
+                                                        <tbody>
+                                                            <tr>
+                                                                <td>1</td>
+                                                                <td>House rent</td>
+                                                                <td>Rent payment for house number <?php echo $payments['house_number']; ?></td>
+                                                                <td>Ksh <?php echo number_format($payments['payment_amount'], 2); ?></td>
+                                                            </tr>
+                                                        </tbody>
+                                                    </table>
+                                                </div>
+                                                <!-- /.col -->
+                                            </div>
+                                            <!-- /.row -->
+
+                                            <div class="row">
+                                                <!-- accepted payments column -->
+                                                <div class="col-6">
+                                                    <p class="lead"></p>
+                                                </div>
+                                                <!-- /.col -->
+                                                <div class="col-6 text-right">
+                                                    <p class="lead">Payment Method: <?php echo $payments['payment_type']; ?></p>
+                                                    <div class="table-responsive">
+                                                        <table class="table">
+                                                            <tr>
+                                                                <th style="width:50%">Subtotal:</th>
+                                                                <td>Ksh <?php echo number_format($payments['payment_amount'], 2); ?></td>
+                                                            </tr>
+                                                            <tr>
+                                                                <th>Total:</th>
+                                                                <td>Ksh <?php echo number_format($payments['payment_amount'], 2); ?></td>
+                                                            </tr>
+                                                        </table>
+                                                    </div>
+                                                </div>
+                                                <!-- /.col -->
+                                            </div>
+                                        </div>
+                                        <!-- /.row -->
+                                        <div class="card-footer">
+                                            <!-- this row will not appear when printing -->
+                                            <div class="row no-print">
+                                                <div class="col-12">
+                                                    <button type="button" class="btn btn-success float-right"><i class="fas fa-print"></i>
+                                                        Print
+                                                    </button>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
