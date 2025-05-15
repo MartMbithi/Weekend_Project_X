3eb9b44515de6f9675d2fdfe6b306d931ead56b8
MartMbithi <martdevelopers254@gmail.com>
2025-05-15 15:59:26 +0300

    Craft Utility Details

--- views/utilities.php
+++ views/utilities.php
@@ -86,12 +86,12 @@ require_once('../partials/head.php');
                 <div class="container">
                     <div class="row mb-2">
                         <div class="col-sm-6">
-                            <h1 class="m-0"> Properties</h1>
+                            <h1 class="m-0"> Utilities</h1>
                         </div><!-- /.col -->
                         <div class="col-sm-6">
                             <ol class="breadcrumb float-sm-right">
                                 <li class="breadcrumb-item"><a href="dashboard">Home</a></li>
-                                <li class="breadcrumb-item"><a href="properties">Properties</a></li>
+                                <li class="breadcrumb-item"><a href="utilities">Utilities</a></li>
                                 <li class="breadcrumb-item active">Manage</li>
                             </ol>
                         </div><!-- /.col -->
@@ -105,7 +105,7 @@ require_once('../partials/head.php');
                 <div class="container">
                     <div class="d-flex flex-row-reverse bd-highlight">
                         <button type="button" data-toggle="modal" data-target="#add_modal" class="btn btn-outline-success">
-                            Add property
+                            Add utility
                         </button>
                     </div>
                     <hr>
@@ -115,7 +115,7 @@ require_once('../partials/head.php');
                             <div class="modal-content">
                                 <div class="modal-header align-items-center">
                                     <div class="text-center">
-                                        <h6 class="mb-0 text-bold">Register new property</h6>
+                                        <h6 class="mb-0 text-bold">Register new utility</h6>
                                     </div>
                                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                         <span aria-hidden="true">&times;</span>
@@ -125,33 +125,25 @@ require_once('../partials/head.php');
                                     <form class="needs-validation" method="post" enctype="multipart/form-data" role="form">
                                         <div class="row">
                                             <div class="form-group col-md-6">
-                                                <label for="">Property name</label>
-                                                <input type="text" required name="property_name" class="form-control">
+                                                <label for="">Utilities name</label>
+                                                <input type="text" required name="utility_name" class="form-control">
                                             </div>
-                                            <div class="form-group col-md-6">
-                                                <label for="">Property location</label>
-                                                <input type="text" required name="property_location" class="form-control">
+                                            <div class="form-group col-md-3">
+                                                <label for="">Utility unit cost (Ksh)</label>
+                                                <input type="text" required name="utility_cost" class="form-control">
                                             </div>
-                                            <div class="form-group col-md-12">
-                                                <label for="">Property caretaker</label>
-                                                <select type="text" required name="property_caretaker_id" class="form-control select2bs4">
-                                                    <option>Select caretaker</option>
-                                                    <?php
-                                                    $users_sql = mysqli_query(
-                                                        $mysqli,
-                                                        "SELECT * FROM users WHERE user_type = 'Caretaker'"
-                                                    );
-                                                    if (mysqli_num_rows($users_sql) > 0) {
-                                                        while ($users = mysqli_fetch_array($users_sql)) {
-                                                    ?>
-                                                            <option value="<?php echo $users['user_id']; ?>"><?php echo $users['user_names']; ?></option>
-                                                    <?php }
-                                                    } ?>
+                                            <div class="form-group col-md-3">
+                                                <label for="">Utility status</label>
+                                                <select name="utility_status" required class="form-control">
+                                                    <option value="">Select utility status</option>
+                                                    <option value="0">Active</option>
+                                                    <option value="1">Inactive</option>
                                                 </select>
                                             </div>
+
                                         </div>
                                         <div class="text-right">
-                                            <button type="submit" name="Add_Property" class="btn btn-outline-success">Add</button>
+                                            <button type="submit" name="Add_Utility" class="btn btn-outline-success">Add</button>
                                         </div>
                                     </form>
                                 </div>
@@ -163,46 +155,47 @@ require_once('../partials/head.php');
                         <div class="col-12 col-sm-12 col-md-12">
                             <div class="card card-outline card-success">
                                 <div class="card-header">
-                                    <h5 class="card-title">Registered properties</h5>
+                                    <h5 class="card-title">Registered utilities</h5>
                                 </div>
                                 <div class="card-body">
                                     <table class="table data_table table-striped">
                                         <thead>
                                             <tr>
                                                 <th>S/no</th>
-                                                <th>Property Name</th>
-                                                <th>Property Location</th>
-                                                <th>Caretaker</th>
+                                                <th>Utility name</th>
+                                                <th>Utility cost</th>
+                                                <th>Utility status</th>
                                                 <th>Manage</th>
                                             </tr>
                                         </thead>
                                         <tbody>
                                             <?php
-                                            $property_sql = mysqli_query(
+                                            $utilities_sql = mysqli_query(
                                                 $mysqli,
-                                                "SELECT * FROM properties p
-                                                INNER JOIN users u ON u.user_id = p.property_caretaker_id"
+                                                "SELECT * FROM utilities"
                                             );
-                                            if (mysqli_num_rows($property_sql) > 0) {
+                                            if (mysqli_num_rows($utilities_sql) > 0) {
                                                 $cnt = 1;
-                                                while ($properties = mysqli_fetch_array($property_sql)) {
+                                                while ($utilities = mysqli_fetch_array($property_sql)) {
+                                                    if ($utilities['utility_status'] == 0) {
+                                                        $utility_status = '<span class="badge badge-success">Active</span>';
+                                                    } else {
+                                                        $utility_status = '<span class="badge badge-danger">Inactive</span>';
+                                                    }
                                             ?>
                                                     <tr>
                                                         <td><?php echo $cnt; ?></td>
-                                                        <td><?php echo $properties['property_name']; ?></td>
-                                                        <td><?php echo $properties['property_location']; ?></td>
-                                                        <td><?php echo $properties['user_names']; ?></td>
+                                                        <td><?php echo $utilities['utility_name']; ?></td>
+                                                        <td>Ksh <?php echo number_format($utilities['utility_cost']); ?></td>
+                                                        <td><?php echo $utility_status; ?></td>
                                                         <td>
-                                                            <a data-toggle="modal" href="#add_<?php echo $properties['property_id']; ?>" class="badge badge-success"><i class="fas fa-plus"></i> Add house</a>
-                                                            <a data-toggle="modal" href="#update_<?php echo $properties['property_id']; ?>" class="badge badge-primary"><i class="fas fa-edit"></i> Edit</a>
-                                                            <?php if ($_SESSION['user_type'] == 'Administrator') { ?>
-                                                                <a data-toggle="modal" href="#delete_<?php echo $properties['property_id']; ?>" class="badge badge-danger"><i class="fas fa-trash"></i> Delete</a>
-                                                            <?php } ?>
+                                                            <a data-toggle="modal" href="#update_<?php echo $utilities['utility_id']; ?>" class="badge badge-primary"><i class="fas fa-edit"></i> Edit</a>
+                                                            <a data-toggle="modal" href="#delete_<?php echo $utilities['utility_id']; ?>" class="badge badge-danger"><i class="fas fa-trash"></i> Delete</a>
                                                         </td>
                                                     </tr>
                                             <?php
                                                     $cnt = $cnt + 1;
-                                                    include('../modals/property.php');
+                                                    include('../modals/utilities.php');
                                                 }
                                             } ?>
                                         </tbody>
