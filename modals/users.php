a0a5793604e3f1411dfabcde3651b884d843b793
MartMbithi <martdevelopers254@gmail.com>
2025-05-16 12:44:40 +0300

    Craft Users Details

--- modals/users.php
+++ modals/users.php
@@ -13,10 +13,14 @@
             <div class="modal-body">
                 <form class="needs-validation" method="post" enctype="multipart/form-data" role="form">
                     <div class="row">
-                        <div class="form-group col-md-12">
+                        <div class="form-group col-md-6">
                             <label for="">Full names</label>
                             <input type="hidden" required name="user_id" value="<?php echo $users['user_id']; ?>" class="form-control">
                             <input type="text" required name="user_names" value="<?php echo $users['user_names']; ?>" class="form-control">
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label for="">Contacts</label>
+                            <input type="text" required name="user_contact" value="<?php echo $users['user_contact']; ?>" class="form-control">
                         </div>
                         <div class="form-group col-md-6">
                             <label for="">Access level</label>
