97a329f3e8ed98481a13a3b5c14a8e683972a9af
MartMbithi <martdevelopers254@gmail.com>
2025-05-16 12:53:19 +0300

    Craft Expenses Details

--- helpers/expenses.php
+++ helpers/expenses.php
@@ -11,9 +11,11 @@ if (isset($_POST['Add_Expense'])) {
     $expense_type = mysqli_real_escape_string($mysqli, $_POST['expense_type']);
     $expense_amount = mysqli_real_escape_string($mysqli, $_POST['expense_amount']);
     $expense_date = mysqli_real_escape_string($mysqli, $_POST['expense_date']);
+    $expense_property_id = mysqli_real_escape_string($mysqli, $_POST['expense_property_id']);
+    $expense_house_number = mysqli_real_escape_string($mysqli, $_POST['expense_house_number']);
 
-    $add_expense_sql = "INSERT INTO expenses (expense_user_id, expense_type, expense_amount, expense_date)
-     VALUES ('{$expense_user_id}', '{$expense_type}', '{$expense_amount}', '{$expense_date}')";
+    $add_expense_sql = "INSERT INTO expenses (expense_user_id, expense_type, expense_amount, expense_date, expense_property_id, expense_house_number)
+     VALUES ('{$expense_user_id}', '{$expense_type}', '{$expense_amount}', '{$expense_date}', '{$expense_property_id}', '{$expense_house_number}')";
 
     if (mysqli_query($mysqli, $add_expense_sql)) {
         $success = "Expense added";
@@ -27,10 +29,14 @@ if (isset($_POST['Update_Expense'])) {
     $expense_type = mysqli_real_escape_string($mysqli, $_POST['expense_type']);
     $expense_amount = mysqli_real_escape_string($mysqli, $_POST['expense_amount']);
     $expense_date = mysqli_real_escape_string($mysqli, $_POST['expense_date']);
+    $expense_property_id = mysqli_real_escape_string($mysqli, $_POST['expense_property_id']);
+    $expense_house_number = mysqli_real_escape_string($mysqli, $_POST['expense_house_number']);
     $expense_id = mysqli_real_escape_string($mysqli, $_POST['expense_id']);
 
     /* Persist */
-    $update_sql = "UPDATE expenses SET expense_type = '{$expense_type}', expense_amount = '{$expense_amount}', expense_date = '{$expense_date}' WHERE expense_id = '{$expense_id}'";
+    $update_sql = "UPDATE expenses SET expense_type = '{$expense_type}', expense_amount = '{$expense_amount}', 
+    expense_date = '{$expense_date}', expense_property_id = '{$expense_property_id}', expense_house_number = '{$expense_house_number}'
+    WHERE expense_id = '{$expense_id}'";
 
     if (mysqli_query($mysqli, $update_sql)) {
         $success = "Expense updated";
