56e1ab58c40ed7092f9ae9b6b8291a9ed35b958b
Mart Mbithi <martdevelopers254@gmail.com>
2025-05-17 10:35:58 +0300

    Craft Houses - Add Tenant Details

--- helpers/houses.php
+++ helpers/houses.php
@@ -164,3 +164,36 @@ if (isset($_POST['Delete_House'])) {
         $error = "Error deleting house";
     }
 }
+
+/* Add Tenant */
+if (isset($_POST['Add_Tenants'])) {
+    $tenant_national_id  = mysqli_real_escape_string($mysqli, $_POST['tenant_national_id']);
+    $tenant_name = mysqli_real_escape_string($mysqli, $_POST['tenant_name']);
+    $tenant_mobile_number = mysqli_real_escape_string($mysqli, $_POST['tenant_mobile_number']);
+    $tenant_house_id = mysqli_real_escape_string($mysqli, $_POST['tenant_house_id']);
+    $tenant_date_of_registration = mysqli_real_escape_string($mysqli, $_POST['tenant_date_of_registration']);
+    $house_status = mysqli_real_escape_string($mysqli, 'Occupied');
+
+
+    /* Prevent Duplicate Tenants */
+    /* 
+    This will trigger a cascading effect, assume a tenant leaves then comes back and allocated same room number he was in it initially
+    $check_tenant = mysqli_query($mysqli, 
+    "SELECT * FROM tenants WHERE tenant_national_id = '{$tenant_national_id'
+    AND tenant_house_id = '$tenant_house_id'");
+    ");
+     */
+    /* Persist */
+    $add_tenant = "INSERT INTO tenants (tenant_national_id, tenant_name, tenant_mobile_number, tenant_house_id, tenant_date_of_registration)
+    VALUES ('{$tenant_national_id}', '{$tenant_name}', '{$tenant_mobile_number}', '{$tenant_house_id}', '{$tenant_date_of_registration}')";
+    if (mysqli_query($mysqli, $add_tenant)) {
+        $update_house_status = "UPDATE houses SET house_status = '{$house_status}' WHERE house_id = '{$tenant_house_id}'";
+        if (mysqli_query($mysqli, $update_house_status)) {
+            $success = "Tenant Added";
+        } else {
+            $error = "House allocation failed";
+        }
+    } else {
+        $error = "Tenant addition failed";
+    }
+}
